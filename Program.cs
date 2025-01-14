042997c01229cb1611fc0a60673a7f2b29ee9d6c
Devconte <romain.lcnte@gmail.com>
2025-01-14 15:32:01 +0100

    Base project + animal class and lion subclass

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿public abstract class Animal
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+
+    public Animal(string name, double weight)
+    {
+        Name = name;
+        Weight = weight;
+    }
+    
+    public abstract void MakeNoise();
+}
+
+
+public class Lion : Animal
+{
+    public Lion(string name, double weight):base(name, weight) {} // base is used to call the constructor of class parent.
+    public override void MakeNoise()
+    {
+        Console.WriteLine("Roar");
+    }
+}
