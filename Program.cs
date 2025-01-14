c7caed5c85e0879bea1f1103e390aa1bda6e522b
Devconte <romain.lcnte@gmail.com>
2025-01-14 16:21:46 +0100

    added enclosure class

--- Program.cs
+++ Program.cs
@@ -40,3 +40,14 @@ public class Kangaroo : Animal
         Console.WriteLine("Kangaroo");
     }
 }
+
+public class Enclosure
+{
+    public string Name { get; set; }
+    public List<Animal> Animals { get; set; } = new List<Animal>();
+
+    public void AddAnimal(Animal animal)
+    {
+        Animals.Add(animal);
+    }
+}
