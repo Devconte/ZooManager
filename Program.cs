9fa405c622ef0d47ee935189aff422eb5c8dd84e
Devconte <romain.lcnte@gmail.com>
2025-01-14 16:47:31 +0100

    added program zoo

--- Program.cs
+++ Program.cs
@@ -52,6 +52,8 @@ public class Enclosure
     }
 }
 
+public delegate void AlertHandler(string message);
+
 public class Zoo
 {
     public List<Animal> Animals { get; set; } = new List<Animal>();
@@ -66,4 +68,28 @@ public interface ICarnivore
 public interface IHerbivore
 {
     void Graze();
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Zoo myZoo = new Zoo();
+        
+        var lion = new Lion("Simba", 180);
+        var elephant = new Elephant("Dumbo", 180);
+        var kangaroo = new Kangaroo("Roger", 180);
+        
+        Enclosure cSavannah = new Enclosure { Name = "cSavannah" };
+        Enclosure oceania  = new Enclosure { Name = "Oceania" };
+        Enclosure hSavannah = new Enclosure { Name = "hSavannah" };
+        
+        myZoo.Enclosures["cSavannah"] = cSavannah;
+        myZoo.Enclosures["oceania"] = oceania;
+        myZoo.Enclosures["hSavannah"] = hSavannah;
+        
+        cSavannah.AddAnimal(lion);
+        oceania.AddAnimal(kangaroo);
+        hSavannah.AddAnimal(elephant);
+    }
 }
