9235ab953b018bee25281ed581a8f782aeac1ba9
Devconte <romain.lcnte@gmail.com>
2025-01-15 10:46:49 +0100

    added queue to Zoo

--- Services/Zoo.cs
+++ Services/Zoo.cs
@@ -6,4 +6,11 @@ public class Zoo
 {
     public List<Animal> Animals { get; set; } = new List<Animal>();
     public Dictionary<string, Enclosure> Enclosures { get; set; } = new Dictionary<string, Enclosure>();
+    
+    public Queue<string> VisitorsQueue { get; set; } = new Queue<string>();
+
+    public void AddVisitor(string visitor)
+    {
+        VisitorsQueue.Enqueue(visitor);
+    }
 }
