8ce0ceb1871ed66baf658949dc67f0d6de449b5f
Devconte <romain.lcnte@gmail.com>
2025-01-15 10:46:37 +0100

    added alert usage to Enclosure.cs

--- Models/Enclosure.cs
+++ Models/Enclosure.cs
@@ -1,11 +1,23 @@
+using ZooManager.Delegates;
+
 namespace ZooManager.Models;
 public class Enclosure
 {
     public string Name { get; set; }
     public List<Animal> Animals { get; set; } = new List<Animal>();
+    private const int MaxCapacity = 3;
+
+    public event AlertHandler Alert;
 
     public void AddAnimal(Animal animal)
     {
-        Animals.Add(animal);
+        if (Animals.Count >= MaxCapacity)
+        {
+            Alert?.Invoke($"Enclosure {Name} has reached the maximum number of animals. !");
+        }
+        else
+        {
+            Animals.Add(animal);
+        }
     }
 }
